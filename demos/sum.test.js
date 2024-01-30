9b4acf8b51e3a758fe7acef93de7e8fd39ffc6aa
Javier <mastos44@gmail.com>
2024-01-30 21:31:11 +0100

    test: Ejemplo basico de test

--- demos/sum.test.js
+++ demos/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test('sumar 1 + 2 es igual a 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+it('sumar 3 + 4 es igual a 7', () => {
+  expect(sum(3, 4)).toBe(7);
+});
