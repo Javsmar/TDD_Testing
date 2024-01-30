52d624e50f05003ca28cae3641f9d036617719c7
Javier <mastos44@gmail.com>
2024-01-30 21:58:53 +0100

    test: dos suites de test

--- demos/sum.test.js
+++ demos/sum.test.js
@@ -1,9 +1,19 @@
 const sum = require('./sum');
 
-test('sumar 1 + 2 es igual a 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+describe("Función suma", () => {
+  describe("Suma números positivos", () => {
+    it('sumar 1 + 2 es igual a 3', () => {
+      expect(sum(1, 2)).toBe(3);
+    });
+    
+    it('sumar 3 + 4 es igual a 7', () => {
+      expect(sum(3, 4)).toBe(7);
+    });
+  })
+  describe("Suma números negativos", () => {
+    it("sumar 2 y -3 es igual a -1", () => {
+      expect(sum(2, -3)).toBe(-1);
+    });
+  })
 
-it('sumar 3 + 4 es igual a 7', () => {
-  expect(sum(3, 4)).toBe(7);
 });
